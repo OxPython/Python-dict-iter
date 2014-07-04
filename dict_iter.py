4b731371564b0a51ed96803f0d74facb8185aeea
Emerson <root@emerson-N53SN>
2014-07-04 05:26:01 -0500

    HowTo create an iterator from a dict in python?

--- dict_iter.py
+++ dict_iter.py
@@ -1,43 +1,35 @@
 '''
-Created on Jul 1, 2014
+Created on Jul 2, 2014
 
 @author: viejoemer
+
+HowTo create an iterator from a dict in python?
+
+¿Cómo crear un iterador de un diccionario?
+
 '''
-############################################################
-#HowTo create an iterator from a dict?
-
-#Creating a empty dict
-
-d = {}
-
-d = dict()
 
 #Creating a dict with data
-
 d = {
      "red":100,
      "yellow":200,
      "blue" : 300
      }
+print(d)
 
 #Create a iterator of keys
 #iter(d) is similar to iter(d.keys())
 d2 = iter(d)
-
 print(d2)
 
 for i in d2:
     print(i)
-
 
 #Create a iterator of values
-
 d2 = iter(d.values())
-
 print(d2)
 
 for i in d2:
     print(i)
-    
-print(d)
+
 
